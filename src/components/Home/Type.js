2602877356179f787f625b0c8e8aeab34865d233
Stavros Giannis <hackerone1@web.de>
2024-08-13 18:13:32 +0200

    type strings changed

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Software Engineer",
+          "Cyber Security Specialist",
+          "ISO 27001 Auditor",
         ],
         autoStart: true,
         loop: true,
